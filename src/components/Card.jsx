003ffbcdf60f63b937d8067a2fad01e702f3de7d
Sp17f1re <a.hutchison@live.co.uk>
2020-06-09 21:18:47 -0700

    Initial commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Avatar from "./Avatar";
+import Detail from "./Detail";
+
+function Card(props) {
+  return (
+    <div className="card">
+      <div className="top">
+        <Detail detail={props.id} />
+        <h2 className="name">{props.name}</h2>
+        <Avatar imgURL={props.imgURL} />
+      </div>
+      <div className="bottom">
+        <Detail detail={props.phone} />
+        <Detail detail={props.email} />
+      </div>
+    </div>
+  );
+}
+
+export default Card;
